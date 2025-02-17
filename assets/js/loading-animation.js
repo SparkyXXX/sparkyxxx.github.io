69b084707f77ede4a09cefaffe6c3480cf818627
3113624526@qq.com <3113624526@qq.com>
2025-02-17 10:00:26 +0800

    根据主题更换加载动画

--- assets/js/loading-animation.js
+++ assets/js/loading-animation.js
@@ -1,7 +1,19 @@
-window.addEventListener('load', function () {
+const lightAnimationContainer =
+  document.getElementsByClassName('light-animation');
+const darkAnimationContainer =
+  document.getElementsByClassName('dark-animation');
+console.log('111', Theme.visualState);
+if (Theme.visualState === 'light') {
+  lightAnimationContainer[0].style.display = 'flex';
+  darkAnimationContainer[0].style.display = 'none';
+} else if (Theme.visualState === 'dark') {
+  darkAnimationContainer[0].style.display = 'flex';
+  lightAnimationContainer[0].style.display = 'none';
+}
+
+setTimeout(() => {
   const loadingContainer = document.querySelector('.loading-container');
   const mainContent = document.querySelector('.main-container');
-  console.log('页面加载完成');
   loadingContainer.style.display = 'none';
   mainContent.style.display = 'block';
-});
+}, 3000);
