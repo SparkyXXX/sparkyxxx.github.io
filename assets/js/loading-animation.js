ba164a704442b92c9c454123ad1dd26d5120bfd8
3113624526 <3113624526@qq.com>
2025-08-17 20:40:44 +0800

    整理博客，调整为深色模式

--- assets/js/loading-animation.js
+++ assets/js/loading-animation.js
@@ -2,13 +2,15 @@ const lightAnimationContainer =
   document.getElementsByClassName('light-animation');
 const darkAnimationContainer =
   document.getElementsByClassName('dark-animation');
-if (Theme.visualState === 'light') {
-  lightAnimationContainer[0].style.display = 'flex';
-  darkAnimationContainer[0].style.display = 'none';
-} else if (Theme.visualState === 'dark') {
-  darkAnimationContainer[0].style.display = 'flex';
-  lightAnimationContainer[0].style.display = 'none';
-}
+// if (Theme.visualState === 'light') {
+//   lightAnimationContainer[0].style.display = 'flex';
+//   darkAnimationContainer[0].style.display = 'none';
+// } else if (Theme.visualState === 'dark') {
+//   darkAnimationContainer[0].style.display = 'flex';
+//   lightAnimationContainer[0].style.display = 'none';
+// }
+darkAnimationContainer[0].style.display = 'flex';
+lightAnimationContainer[0].style.display = 'none';
 
 window.addEventListener('load', function () {
   const loadingContainer = document.querySelector('.loading-container');
